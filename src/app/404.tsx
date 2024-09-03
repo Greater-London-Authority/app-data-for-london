4b5e8cd746a382cc88c9ec00014d5e8600571122
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-03 13:12:00 +0100

    add custom 404

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -1,12 +1,12 @@
 import MarkdownRenderer from '@/components/MarkdownRenderer';
 import { getMarkdownContent } from '@/lib/MarkdownToHtml';
 
-export default async function Home() {
-	const content = await getMarkdownContent('index.md');
+export default async function NotFound() {
+	const content = await getMarkdownContent('404.md');
 
 	return (
 		<div className='p-24'>
-			<div className='block text-justify max-w-7xl mx-auto'>
+			<div className='block text-center max-w-7xl mx-auto'>
 				<MarkdownRenderer content={content} />
 			</div>
 		</div>
