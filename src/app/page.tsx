52ab56086c4112b84faf1af8b086e8a12dbc72f1
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-23 13:53:15 +0100

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-import MarkdownRenderer from '@/components/MarkdownRenderer';
-import getStrapiData from '@/lib/Strapi';
-
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+import getStrapiData from "@/lib/Strapi";
 
 export default async function Home() {
-	const pageDocumentId = 't3o5oqd1uopm0fjxvy6fh3cd'
-	const { data } = await getStrapiData(`/api/pages/${pageDocumentId}`)
-	const MDContent = data.rich_text
+  const pageDocumentId = "t3o5oqd1uopm0fjxvy6fh3cd";
+  const { data } = await getStrapiData(`/api/pages/${pageDocumentId}`);
+  const MDContent = data.rich_text;
 
-	return (
-		<div className='py-24'>
-			<div className='block text-justify max-w-7xl mx-auto'>
-				<div>
-					
-				<div>
-				<h2>{data.title}</h2>
-				<MarkdownRenderer content={MDContent} />
-				</div>
-    </div>	
-			</div>
-		</div>
-	);
+  return (
+    <div className="py-24">
+      <div className="block text-justify max-w-7xl mx-auto">
+        <div>
+          <div>
+            <h2>{data.title}</h2>
+            <MarkdownRenderer content={MDContent} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/lib/Strapi.ts
+++ src/lib/Strapi.ts
@@ -1,31 +1,35 @@
 import qs from "qs";
-export default async function getStrapiData(path: string, urlParamsObject: object = {}, options: object = {}) {
-	const baseUrl = 'http://localhost:1337';
+export default async function getStrapiData(
+  path: string,
+  urlParamsObject: object = {},
+  options: object = {}
+) {
+  const baseUrl = "http://localhost:1337";
 
-    const url = new URL(path, baseUrl)
-    
+  const url = new URL(path, baseUrl);
 
-	try {
-        // Merge default and user options
-        const mergedOptions = {
-            next: { revalidate: 60 },
-            headers: {
-              "Content-Type": "application/json",
-            },
-            ...options,
-          };
-      
-        // Build request URL
-        const queryString = qs.stringify(urlParamsObject);
-        const requestUrl = `${baseUrl}/api${path}${queryString ? `?${queryString}` : ""}`;
+  try {
+    // Merge default and user options
+    const mergedOptions = {
+      next: { revalidate: 60 },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      ...options,
+    };
 
-        // API call
-		const response = await fetch(baseUrl + path, mergedOptions);
-		const data = await response.json();
-		console.log(data)
-		return data;
+    // Build request URL
+    const queryString = qs.stringify(urlParamsObject);
+    const requestUrl = `${baseUrl}/api${path}${
+      queryString ? `?${queryString}` : ""
+    }`;
 
-	} catch (error) {
-		console.error(error);
-	}
+    // API call
+    const response = await fetch(baseUrl + path, mergedOptions);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
   }
+}
