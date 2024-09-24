fc4d2a03780a8dfa0a4a5500abf24828a21c4b7b
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-24 13:10:25 +0100

    fetch faqs

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,20 +1,23 @@
 import Breadcrumbs from '@/components/Breadcrumbs';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
-import { getMarkdownContent } from '@/lib/MarkdownToHtml';
+import getStrapiData from '@/lib/Strapi';
+
+const pageDocumentId = 'b32r9zfgy8gefbyut416v8p0';
+const path = `/api/pages/${pageDocumentId}`;
 
 export default async function FAQ() {
-	const content = await getMarkdownContent('faq.md');
-	const breadcrumbs = [
-		{ label: 'Home', href: '/' },
-		{ label: 'FAQs' }, // No href means it's the current page
-	];
+  const { data } = await getStrapiData(path);
 
-	return (
-		<div>
-			<Breadcrumbs items={breadcrumbs} />
-			<div className='py-24 block text-justify max-w-7xl mx-auto'>
-				<MarkdownRenderer content={content} />
-			</div>
-		</div>
-	);
+  const breadcrumbs = [
+    { label: 'Home', href: '/' },
+    { label: data.breadcrumb }, // No href means it's the current page
+  ];
+
+  return (
+    <div>
+      <Breadcrumbs items={breadcrumbs} />
+      <div className="py-24 block text-justify max-w-7xl mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
+      </div>
+    </div>
+  );
 }
