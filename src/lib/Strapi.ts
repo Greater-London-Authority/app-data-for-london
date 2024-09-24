139abcc6db7e3077893bd853a76e3eb6dd54fc02
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-24 10:10:53 +0100

    fetch data in footer

--- src/lib/Strapi.ts
+++ src/lib/Strapi.ts
@@ -1,12 +1,11 @@
 import qs from 'qs';
+
 export default async function getStrapiData(
   path: string,
   urlParamsObject: object = {},
   options: object = {}
 ) {
   const baseUrl = 'http://localhost:1337';
-
-  const url = new URL(path, baseUrl);
 
   try {
     // Merge default and user options
@@ -20,14 +19,13 @@ export default async function getStrapiData(
 
     // Build request URL
     const queryString = qs.stringify(urlParamsObject);
-    const requestUrl = `${baseUrl}/api${path}${
+    const requestUrl = `${baseUrl}${path}${
       queryString ? `?${queryString}` : ''
     }`;
 
     // API call
-    const response = await fetch(baseUrl + path, mergedOptions);
+    const response = await fetch(requestUrl, mergedOptions);
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
