e485c616c37f2380778697eb10734b28039a5a2a
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-24 11:10:23 +0100

    fetch data nad populate navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,63 +1,58 @@
 import Link from 'next/link';
 import React from 'react';
 import Image from 'next/image';
+import getStrapiData from '@/lib/Strapi';
 
-const Nav: React.FC = () => {
-	return (
-		<div className='w-full flex justify-center text-darkpink px-24'>
-			<nav className='flex flex-wrap justify-between items-center py-4 max-w-7xl w-full'>
-				<div className='flex flex-wrap items-center'>
-					<Link href='/'>
-						<Image src='' alt='Data for London' width={70} height={70} />
-					</Link>
-				</div>
-				<ul className='flex flex-col md:flex-row justify-between gap-9 list-none'>
-					<li>
-						<Link href='/'>
-							<span className='flex items-center font-semibold'>
-								Home
-								<Image
-									src=''
-									alt='home icon'
-									width={20}
-									height={20}
-									className='ml-2'
-								/>
-							</span>
-						</Link>
-					</li>
-					<li>
-						<Link href='/search'>
-							<span className='flex items-center font-semibold'>
-								Search
-								<Image
-									src=''
-									alt='search icon'
-									width={20}
-									height={20}
-									className='ml-2'
-								/>
-							</span>
-						</Link>
-					</li>
-					<li>
-						<Link href='/login'>
-							<span className='flex items-center font-semibold'>
-								Log in
-								<Image
-									src=''
-									alt='log in icon'
-									width={20}
-									height={20}
-									className='ml-2'
-								/>
-							</span>
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
+interface NavigationLink {
+  id: number;
+  href: string;
+  label: string;
+  target: string | null;
+  isExternal: boolean;
+}
+
+interface NavigationData {
+  links: NavigationLink[];
+}
+
+const navPath = `/api/global`;
+const navParams = {
+  populate: {
+    navigation: {
+      populate: ['links'],
+    },
+  },
 };
 
-export default Nav;
+export default async function Nav() {
+  const { data } = await getStrapiData(navPath, navParams);
+  const navigationData: NavigationData = data.navigation;
+  console.log(navigationData);
+
+  return (
+    <div className="w-full flex justify-center text-darkpink px-24">
+      <nav className="flex flex-wrap justify-between items-center py-4 max-w-7xl w-full">
+        <div className="flex flex-wrap items-center">
+          <Link href="/">
+            <Image src="" alt="Data for London" width={70} height={70} />
+          </Link>
+        </div>
+        <ul className="flex flex-col md:flex-row justify-between gap-9 list-none">
+          {navigationData.links.map((link: NavigationLink) => (
+            <li key={link.id}>
+              <Link
+                href={link.href}
+                target={link.target || undefined}
+                rel={link.isExternal ? 'noopener noreferrer' : undefined}
+              >
+                <span className="flex items-center font-semibold">
+                  {link.label}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
