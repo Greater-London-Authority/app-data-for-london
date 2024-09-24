129938735952c597a09cd65338d63f1206c6b58b
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-24 15:26:38 +0100

    fetch terms and conditions

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -1,20 +1,23 @@
 import Breadcrumbs from '@/components/Breadcrumbs';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
-import { getMarkdownContent } from '@/lib/MarkdownToHtml';
+import getStrapiData from '@/lib/Strapi';
+
+const pageDocumentId = 'eo7k7zn7d317dm5ti0pyqssw';
+const path = `/api/pages/${pageDocumentId}`;
 
 export default async function TermsAndConditions() {
-	const content = await getMarkdownContent('terms-and-conditions.md');
-	const breadcrumbs = [
-		{ label: 'Home', href: '/' },
-		{ label: 'Terms and conditions' }, // No href means it's the current page
-	];
+  const { data } = await getStrapiData(path);
 
-	return (
-		<div>
-			<Breadcrumbs items={breadcrumbs} />
-			<div className='py-24 block text-justify max-w-7xl mx-auto'>
-				<MarkdownRenderer content={content} />
-			</div>
-		</div>
-	);
+  const breadcrumbs = [
+    { label: 'Home', href: '/' },
+    { label: data.breadcrumb }, // No href means it's the current page
+  ];
+
+  return (
+    <div>
+      <Breadcrumbs items={breadcrumbs} />
+      <div className="py-24 block text-justify max-w-7xl mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
+      </div>
+    </div>
+  );
 }
