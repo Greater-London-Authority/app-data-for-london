7f6d269f6e939b21555dfe40039f43c5f05e5be1
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-24 14:24:10 +0100

    fetch privacy-policy and style links

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,20 +1,23 @@
 import Breadcrumbs from '@/components/Breadcrumbs';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
-import { getMarkdownContent } from '@/lib/MarkdownToHtml';
+import getStrapiData from '@/lib/Strapi';
+
+const pageDocumentId = 'klp5uoooc0965w5n1a5odb5n';
+const path = `/api/pages/${pageDocumentId}`;
 
 export default async function PrivacyPolicy() {
-	const content = await getMarkdownContent('privacy-policy.md');
-	const breadcrumbs = [
-		{ label: 'Home', href: '/' },
-		{ label: 'Privacy Policy' }, // No href means it's the current page
-	];
+  const { data } = await getStrapiData(path);
 
-	return (
-		<div>
-			<Breadcrumbs items={breadcrumbs} />
-			<div className='py-24 block text-justify max-w-7xl mx-auto'>
-				<MarkdownRenderer content={content} />
-			</div>
-		</div>
-	);
+  const breadcrumbs = [
+    { label: 'Home', href: '/' },
+    { label: data.breadcrumb }, // No href means it's the current page
+  ];
+
+  return (
+    <div>
+      <Breadcrumbs items={breadcrumbs} />
+      <div className="py-24 block text-justify max-w-7xl mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
+      </div>
+    </div>
+  );
 }
