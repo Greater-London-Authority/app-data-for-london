ab568e412292567d7781e04a2119c3e4d10a9de8
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-09-24 14:07:57 +0100

    fetch contribute page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,23 @@
 import Breadcrumbs from '@/components/Breadcrumbs';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
-import { getMarkdownContent } from '@/lib/MarkdownToHtml';
+import getStrapiData from '@/lib/Strapi';
+
+const pageDocumentId = 'gk5nghei94gss0arsbdp3fnr';
+const path = `/api/pages/${pageDocumentId}`;
 
 export default async function Contact() {
-	const content = await getMarkdownContent('contact.md');
-	const breadcrumbs = [
-		{ label: 'Home', href: '/' },
-		{ label: 'Contact' }, // No href means it's the current page
-	];
+  const { data } = await getStrapiData(path);
 
-	return (
-		<div>
-			<Breadcrumbs items={breadcrumbs} />
-			<div className='py-24 block text-justify max-w-7xl mx-auto'>
-				<MarkdownRenderer content={content} />
-			</div>
-		</div>
-	);
+  const breadcrumbs = [
+    { label: 'Home', href: '/' },
+    { label: data.breadcrumb }, // No href means it's the current page
+  ];
+
+  return (
+    <div>
+      <Breadcrumbs items={breadcrumbs} />
+      <div className="py-24 block text-justify max-w-7xl mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
+      </div>
+    </div>
+  );
 }
--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -1,20 +1,23 @@
 import Breadcrumbs from '@/components/Breadcrumbs';
-import MarkdownRenderer from '@/components/MarkdownRenderer';
-import { getMarkdownContent } from '@/lib/MarkdownToHtml';
+import getStrapiData from '@/lib/Strapi';
+
+const pageDocumentId = 'fd0qwoyh2pp27xafygpgfpkg';
+const path = `/api/pages/${pageDocumentId}`;
 
 export default async function Contribute() {
-	const content = await getMarkdownContent('how-to-contribute.md');
-	const breadcrumbs = [
-		{ label: 'Home', href: '/' },
-		{ label: 'How to contribute' }, // No href means it's the current page
-	];
+  const { data } = await getStrapiData(path);
 
-	return (
-		<div>
-			<Breadcrumbs items={breadcrumbs} />
-			<div className='py-24 block text-justify max-w-7xl mx-auto'>
-				<MarkdownRenderer content={content} />
-			</div>
-		</div>
-	);
+  const breadcrumbs = [
+    { label: 'Home', href: '/' },
+    { label: data.breadcrumb }, // No href means it's the current page
+  ];
+
+  return (
+    <div>
+      <Breadcrumbs items={breadcrumbs} />
+      <div className="py-24 block text-justify max-w-7xl mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
+      </div>
+    </div>
+  );
 }
