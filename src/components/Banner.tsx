c620cf5a158f2099bd169ab77048240d1d8dbc1b
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-10-04 12:58:09 +0100

    add logo image and fix src references

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import Image from 'next/image';
 
 const Banner: React.FC = () => {
-	return (
-		<div className="w-full">
-			<div className='flex flex-wrap justify-center items-center gap-6 min-h-24 bg-ldn-grey py-6'>
-				<a href='https://www.london.gov.uk/'>
-					<Image
-						className='m-3'
-						src='/logo-mayoral.svg'
-						alt='Mayor of London'
-						width={207}
-						height={16}
-					></Image>
-				</a>
-				<a href='https://www.london.gov.uk/'>
-					<Image
-						className='m-3'
-						src='/logo-assembly.svg'
-						alt='London Assembly'
-						width={194}
-						height={16}
-					></Image>
-				</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className='w-full'>
+      <div className='flex flex-wrap justify-center items-center gap-6 min-h-24 bg-ldn-grey py-6'>
+        <a href='https://www.london.gov.uk/'>
+          <Image
+            className='m-3'
+            src='logo-mayoral.svg'
+            alt='Mayor of London'
+            width={207}
+            height={16}
+          ></Image>
+        </a>
+        <a href='https://www.london.gov.uk/'>
+          <Image
+            className='m-3'
+            src='logo-assembly.svg'
+            alt='London Assembly'
+            width={194}
+            height={16}
+          ></Image>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
