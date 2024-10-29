86a2eb66083421c8adb9e8e013ddd7936d771b4f
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-10-29 14:55:47 +0000

    add Hero element

---
+++ src/components/Hero.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface HeroProps {
+  imageUrl: string;
+  imageAlt?: string;
+  headerText: string;
+  paragraph?: string;
+  buttonHref?: string;
+  buttonText?: string;
+}
+
+const Hero: React.FC<HeroProps> = ({
+  imageUrl,
+  imageAlt = 'background image',
+  headerText,
+  paragraph,
+  buttonHref = '',
+  buttonText = '',
+}) => {
+  return (
+    <div className='relative flex items-center justify-center w-full min-h-[500px]'>
+      <Image
+        src={imageUrl}
+        alt={imageAlt}
+        fill
+        style={{ objectFit: 'cover' }}
+        className='absolute inset-0 -z-10'
+      />
+      <div className='max-w-[1200px] p-4 w-full text-white flex flex-col'>
+        <h1 className='text-[120px]'>{headerText}</h1>
+        <div className='w-full flex justify-between items-start flex-wrap'>
+          {paragraph && <p className='text-xl mb-6 md:w-1/2'>{paragraph}</p>}
+          {buttonHref && (
+            <a href={buttonHref} className='btn'>
+              {buttonText}
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
